ad67bf81eb02ce7feb3932f0a616f8e7aa4764a0
Your Name <your.email@example.com>
2024-09-12 11:04:29 +0600

    Solving Problem

---
+++ Xorrrr.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int countDifferentBits(int a, int b) {
+    int xorResult = a ^ b;
+
+    int count = 0;
+    while (xorResult != 0) {
+        if (xorResult & 1) {
+            count++;
+        }
+        xorResult >>= 1;
+    }
+
+    return count;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    int n,m,k;
+    cin>>n>>m>>k;
+    vector<int>vec(m+1);
+
+    for(int i=0; i<m+1; i++) {
+        cin>>vec[i];
+    }
+
+        int count = 0;
+    for(int i=0; i<m; i++) {
+        int result = countDifferentBits(vec[i],vec[m]);
+        if(result<=k)count++;
+    }cout<<count<<endl;
+
+}
+
+
